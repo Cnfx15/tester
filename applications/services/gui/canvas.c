ef8169581f308c5d5c3370c636b36db03cb49fee
Cnfx15 <38139157+Cnfx15@users.noreply.github.com>
2023-02-27 15:26:41 -0800

    Update canvas.c

--- applications/services/gui/canvas.c
+++ applications/services/gui/canvas.c
@@ -126,9 +126,9 @@ void canvas_set_font(Canvas* canvas, Font font) {
     furi_assert(canvas);
     u8g2_SetFontMode(&canvas->fb, 1);
     if(font == FontPrimary) {
-        u8g2_SetFont(&canvas->fb, u8g2_font_VCR_OSD_tr);
+        u8g2_SetFont(&canvas->fb, u8g2_font_crox4hb_tf);
     } else if(font == FontSecondary) {
-        u8g2_SetFont(&canvas->fb, u8g2_font_crox4hb_tr);
+        u8g2_SetFont(&canvas->fb, u8g2_font_t0_22b_tr);
     } else if(font == FontKeyboard) {
         u8g2_SetFont(&canvas->fb, u8g2_font_profont11_mr);
     } else if(font == FontBigNumbers) {
